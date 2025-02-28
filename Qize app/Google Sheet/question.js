4f360e1a34577f2a2e7b37034968d0b1f58045c3
Akbarms112 <111769597+Akbarms112@users.noreply.github.com>
2025-02-28 17:28:10 +0530

    Add files via upload

---
+++ Qize app/Google Sheet/question.js
@@ -0,0 +1,59 @@
+let questions =[
+    {
+        
+        numb: 1,
+        question:"What does HTML stand for?",
+        answer:"Hyper Text Markup Language",
+        options:[
+            "Hyper Text preprocessor",
+            "Hyper Text Markup Language",
+            "Hyper Text Multiple Lanuage",
+            "Hyper Text Multi Language"
+        ]
+    },
+    {
+        numb:2,
+        question:"What does CSS stand for?",
+        answer:"Cascading Style Sheet",
+        options:[
+            "Common style sheet",
+            "Colorful Style Sheet",
+            "Computer Style sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb:3,
+        question:"What does PHP stand for?",
+        answer:"Hypertext preprocessor",
+        options:[
+            "Hypertext preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "HomeText preprocessor"
+        ]
+    },
+    {
+        numb:4,
+        question:"What does SQL stand for?",
+        answer:"Structured Query language",
+        options:[
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query language"
+        ]
+    },
+    {
+        numb:5,
+        question:"What does XML stand for?",
+        answer:"extensible Markup Language",
+        options:[
+            "extensible Markup Language",
+            "executable Multiple Language",
+            "extra Multi-program Language",
+            "examine Multiple Language"
+        ]
+        
+    },
+];
